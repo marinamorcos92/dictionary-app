9e0d74f26f754abf2bd43a6b88e433ca0a6921ab
Marina Morcos <marinamorcos@hotmail.com>
2023-05-11 20:06:13 +0200

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    alert(`Searching for ${keyword} definition.`);
+    let apiKey = "4af911bf6cc0a30d4ecc3adao79t646b";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function keywordChange(event) {
     setKeyword(event.target.value);
