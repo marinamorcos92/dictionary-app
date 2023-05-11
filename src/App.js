76612849d173fcc2a54bbd94ccd3ac7769a4e26b
Marina Morcos <marinamorcos@hotmail.com>
2023-05-11 11:24:01 +0200

    added app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import dictionary from "./dictionary.jpeg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dictionary} alt="logo" className="App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
