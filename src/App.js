6cf8b1c8f8af6024bd8dc4160c78958bce3702ee
Marina Morcos <marinamorcos@hotmail.com>
2023-05-11 14:09:19 +0200

    added the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import dictionary from "./dictionary.jpeg";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={dictionary} alt="logo" className="App-logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={dictionary} alt="logo" className="App-logo img-fluid" />
+        </header>
+        <Dictionary />
+        <footer className="text-center">
+          Coded By Marina Morcos{" "}
+          <a
+            href="https://github.com/marinamorcos92/dictionary-app"
+            target="-blank"
+          >
+            Open source code
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function keywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={keywordChange} />
+      </form>
+    </div>
+  );
+}
