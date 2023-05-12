3d2a59d92494c9b4f41648fc0f7d3c28baeb5268
Marina Morcos <marinamorcos@hotmail.com>
2023-05-12 18:40:57 +0200

    adding results and meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -23,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={keywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
